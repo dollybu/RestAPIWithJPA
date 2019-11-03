1157f458dcb906fec5c646d67f49cf68772dc1ce
doll <doll@dollys-air>
2019-11-03 15:13:51 +0530

    initial project setup

---
+++ src/main/java/com/example/testing/MyFirstTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstTestingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/testing/controller/MyController.java
@@ -0,0 +1,23 @@
+package com.example.testing.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@RequestMapping("/test")
+	public String test() {
+		
+	   
+		return "My name is Dolly";
+	}
+	
+	@RequestMapping("/test/{name}")
+	public String test(@PathVariable("name") String nameValue) {
+		
+	   
+		return "My name is "+nameValue;
+	}
+}
